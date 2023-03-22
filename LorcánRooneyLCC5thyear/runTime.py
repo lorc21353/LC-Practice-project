0f0b9723781f54ab812e71a5dc1e13401cd6008e
lorc21353 <114434073+lorc21353@users.noreply.github.com>
2023-03-22 15:00:55 +0000

    Add files via upload

--- LorcánRooneyLCC5thyear/runTime.py
+++ LorcánRooneyLCC5thyear/runTime.py
@@ -3,6 +3,7 @@ import player
 import enemy
 import neat
 import time
+import os
 gameMode = int(input("select gamemode (0 = singleplayer, 1 = multiplayer, 2 = simualtion): "))
 
 runs_per_net = 5
@@ -15,8 +16,8 @@ def eval_genome(genome, config):
     startTime = time.time_ns()/1000000000
     
     net = neat.nn.FeedForwardNetwork.create(genome, config)
-    # as there is more than one run per genome the fitness will need to reflect the average, thus it is placed in a list
-    fitness = []
+    # as there is more than one run per genome the fitness will need to reflect the normal performance, thus it is placed in a list
+    fitnesses = []
     
     # do the required number of runs for each net
     for run in range(runs_per_net):
@@ -27,10 +28,19 @@ def eval_genome(genome, config):
             currTime = time.time_ns()/1000000000     
             inputs = game.getInputs()
             outputs = net.activate(inputs)
+            game.Enemy.movement(outputs)
+            game.draw(0,0,0,0)
+            if game.winOrLose() == -1:
+                fitness = 1000
+            elif game.winOrLose() == -2:
+                fitness = 0
+            else:
+                fitness = -970+game.winOrLose()
+                
+        fitnesses.append(fitness)
         
-        
-        
-        
+    # genomes fitness is its worst across all of its runs
+    return min(fitnesses)
 
 def eval_genomes(genomes, config):
     for genome_id, genome in genomes:
@@ -43,5 +53,27 @@ def eval_genomes(genomes, config):
 # while True:
 #     game1.draw(0,0,10,10)
 #     print(game1.winOrLose())
+
+def run():
+    # Load the config file, which is assumed to live in
+    # the same directory as this script.
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_path)
+
+    pop = neat.Population(config)
+    
+    pop = neat.Population(config)
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+    pop.add_reporter(neat.StdOutReporter(True))
+
+    pe = neat.ParallelEvaluator(1, eval_genome)
+    winner = pop.run(pe.evaluate)
+    print(winner)
     
     
+if __name__ == '__main__':
+    run()    
